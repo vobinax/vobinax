b3a79eb5e2e8996b9745dbe44a667d5e3f23509f
Mohammad Kalhori <141785560+vobinax@users.noreply.github.com>
2023-08-10 00:45:24 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var Name = prompt("Enter Your Name :","Ali")
+
+var age = 25
+
+
+var caption = "NONE"
+
+var online = true
+
+// get type of online variable
+
+alert(`welcome ${Name} !`)
+/*
+first variable
+2st
+3st
+4st
+*/
